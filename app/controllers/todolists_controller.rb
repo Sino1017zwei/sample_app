2b7bb8972ee81cbfb482a76dd0605b77abfb9152
EC2 Default User <ec2-user@ip-172-31-21-223.ec2.internal>
2021-10-01 02:38:49 +0000

    [Finish]3-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -10,7 +10,16 @@ class TodolistsController < ApplicationController
     #2.データをデータベースに登録するためのsaveメソッド実行
     list.save
     #3.完了したらトップ画面にリダイレクト
-    redirect_to '/top'
+    redirect_to todolist_path(list.id)
+  end
+
+  def index
+    @lists=List.all
+  end
+
+  def show
+    @list=List.find(params[:id])#params[:id]=/todolists/:idの:idの部分を指している
+
   end
 
   private #アクション以外はprivate以下に記述を行う
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'todolists/new'=>'todolists#new'
+  get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top'=>'homes#top'
   post 'todolists'=>'todolists#create'
+  get 'todolists'=>'todolists#index'
+  #.../todolists/1 や.../todolists/3に該当する
+  get 'todolists/:id'=>'todolists#show',as:'todolist'#as:名前つきルート
 end
